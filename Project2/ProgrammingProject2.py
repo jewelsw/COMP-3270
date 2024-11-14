c65397b701af39c13c29ad6655bba4061caccf54
jewelsw <ejw0073@auburn.edu>
2024-11-14 16:36:40 -0600

    Track Project2 as a regular file

---
+++ Project2/ProgrammingProject2.py
@@ -0,0 +1,17 @@
+#Social Network Graphical Representation
+#Author: Jewels Wolter
+#Course: COMP 3270
+
+#Import necessary libraries
+import time
+import 
+
+#Parse edges from the file
+
+#Create an adjacency list/array from the edges
+
+#Implement BFS
+
+#Implement DFS
+
+#Generate a report for distance and time taken for BFS and DFS
