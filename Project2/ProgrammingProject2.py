262155bd58005f3b8932774a9b913194bb972aee
jewelsw <ejw0073@auburn.edu>
2024-11-14 16:57:48 -0600

    Upload testing text file and start on getEdges and createGraph methods

--- Project2/ProgrammingProject2.py
+++ Project2/ProgrammingProject2.py
@@ -4,14 +4,35 @@
 
 #Import necessary libraries
 import time
-import 
 
 #Parse edges from the file
+def getEdges(filename):
+    edges = []
+    with open(filename) as file:
+        for line in file:
+            line = line.strip()
+            node1, node2 = line.split(',')
+            edges.append((node1, node2))
+    return edges
 
-#Create an adjacency list/array from the edges
+#Create a graph from the edges
+def createGraph(edges):
+    graph = {}
+    for edge in edges:
+        node1, node2 = edge
+        if node1 not in graph:
+            graph[node1] = []
+        if node2 not in graph:
+            graph[node2] = []
+        graph[node1].append(node2)
+        graph[node2].append(node1)
+    return graph
 
 #Implement BFS
 
 #Implement DFS
 
 #Generate a report for distance and time taken for BFS and DFS
+ 
+#Main function
+print(createGraph(getEdges('Test_Case_Assignment2.txt')))
